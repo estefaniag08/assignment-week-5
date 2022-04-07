d1e097422c5baa94e4f051fd89a9d8731ef79ee2
estefaniag08 <estefaniag08@hotmail.com>
2022-04-06 20:46:14 -0500

    Initialize Spring Boot project with Spring Web, Spring Data and H2

---
+++ src/main/java/dev/applaudostudios/examples/assignmentweek5/AssignmentWeek5Application.java
@@ -0,0 +1,13 @@
+package dev.applaudostudios.examples.assignmentweek5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentWeek5Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/applaudostudios/examples/assignmentweek5/AssignmentWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.applaudostudios.examples.assignmentweek5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentWeek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
